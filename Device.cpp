7a90d8029f2b36176c4245a4477825cf383e95b1
Ulvach <olga.ulvacheva@mail.ru>
2018-12-16 13:48:51 +0300

    create Device class - fields, getters and setters

--- Device.cpp
+++ Device.cpp
@@ -1,1 +1,47 @@
 #include "Device.h"
+
+const string &Device::getProducer() const {
+    return producer;
+}
+
+void Device::setProducer(const string &producer) {
+    Device::producer = producer;
+}
+
+int Device::getCost() const {
+    return cost;
+}
+
+void Device::setCost(int cost) {
+    Device::cost = cost;
+}
+
+const string &Device::getColor() const {
+    return color;
+}
+
+void Device::setColor(const string &color) {
+    Device::color = color;
+}
+
+const string &Device::getInterfaces() const {
+    return interfaces;
+}
+
+void Device::setInterfaces(const string &interfaces) {
+    Device::interfaces = interfaces;
+}
+
+Device::Device(const string &producer, int cost, const string &color, const string &interfaces) : producer(producer),
+                                                                                                  cost(cost),
+                                                                                                  color(color),
+                                                                                                  interfaces(
+                                                                                                          interfaces) {}
+
+Device::~Device() {
+    color = "";
+    producer = "";
+    cost = 0;
+    color = "";
+    interfaces = "";
+}
--- Device.h
+++ Device.h
@@ -1,8 +1,35 @@
 #ifndef TP_DEVICE_H
 #define TP_DEVICE_H
 
+#include <string>
+
+using namespace std;
 
 class Device {
+private:
+    string producer;
+    int cost;
+    string color;
+    string interfaces;
+public:
+    const string &getProducer() const;
+
+    Device(const string &producer, int cost, const string &color, const string &interfaces);
+    virtual ~Device();
+
+    void setProducer(const string &producer);
+
+    int getCost() const;
+
+    void setCost(int cost);
+
+    const string &getColor() const;
+
+    void setColor(const string &color);
+
+    const string &getInterfaces() const;
+
+    void setInterfaces(const string &interfaces);
 
 };
 
